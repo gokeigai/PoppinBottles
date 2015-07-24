5d9a2adbe7cabfded678b29d153cadbf2253b4ea
Timothy Orbasido <t.orbasido@gmail.com>
2015-07-24 21:38:11 +0000

    improve output

--- bottle.rb
+++ bottle.rb
@@ -1,8 +1,9 @@
-require 'pry'
 
 @bonus_soda = 0
 @empty_bottles = 0
 @bottlecaps = 0
+@empty_bottle_bonus = 0
+@bottlecap_bonus = 0
 
 #TODO recursive logic 
 #  cap and bottle amount need to save and record
@@ -11,12 +12,16 @@ require 'pry'
 
 def get_bottle_reward_amount(bottle_amount)
   @empty_bottles = bottle_amount % 2
-  bottle_amount/2
+  current_bonus = bottle_amount/2
+  @empty_bottle_bonus += current_bonus
+  current_bonus
 end
 
 def get_bottlecap_reward_amount(bottlecap_amount)
   @bottlecaps = bottlecap_amount % 4
-  bottlecap_amount/4
+  current_bonus = bottlecap_amount/4
+  @bottlecap_bonus += current_bonus
+  current_bonus
 end
 
 def get_bottle_buy_amount(cash)
@@ -38,11 +43,14 @@ def calculate_final_bottle_amount(bottles)
 
   @bonus_soda += reward
 
-  puts "empty #{@empty_bottles} bottlecap #{@bottlecaps} reward #{@bonus_soda}"
-
   calculate_final_bottle_amount(reward)
 end
 
 # $6 = 3 soda 4 free soda
 first_bottles_amount = get_bottle_buy_amount(6)
-calculate_final_bottle_amount(first_bottles_amount)
+puts "Bottles Bought #{first_bottles_amount}"
+total_reward = calculate_final_bottle_amount(first_bottles_amount)
+puts "Free Bottles Received #{total_reward}"
+puts "Free Bottles Received From Empty Bottles #{@empty_bottle_bonus}"
+puts "Free Bottles Received From Bottlecaps #{@bottlecap_bonus}"
+puts "You have #{@empty_bottles} empty bottles left over and #{@bottlecaps} bottlecaps left over"
